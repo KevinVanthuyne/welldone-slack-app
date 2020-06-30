b476ec21ec1e190abb8feb363cc530a949c64458
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2020-06-30 21:07:47 +0200

    Improved error handling of RewardDao

--- dao/reward_dao.py
+++ dao/reward_dao.py
@@ -12,22 +12,24 @@ class RewardDao():
         self.logger = logging.getLogger()
 
     def _create_connection(self):
-        """ Tries to open a connection to the database and returns the 
+        """ Tries to open a connection to the database and returns the
             connection object. Warning: it does not close the connection,
             this has to be done manually
         """
         try:
             return sqlite3.connect('test.db')
         except Error as e:
-            self.logger.error("Couldn't connect to database:")
-            self.logger.error(e)
+            self.logger.error("Couldn't connect to database: {}".format(e))
 
     def save(self, reward: Reward):
         """ Saves the reward to the database """
-        with self._create_connection() as connection:
-            query = """INSERT INTO rewards (sender, receiver, timestamp)
-                        VALUES (?, ?, ?);"""
-            cursor = connection.cursor()
-            cursor.execute(query, reward)
-            self.logger.info(
-                "Reward {} added to database".format(cursor.lastrowid))
+        try:
+            with self._create_connection() as connection:
+                query = """INSERT INTO rewards (sender, receiver, timestamp)
+                            VALUES (?, ?, ?);"""
+                cursor = connection.cursor()
+                cursor.execute(query, reward)
+                self.logger.info(
+                    "Reward {} added to database".format(cursor.lastrowid))
+        except Error as e:
+            self.logger.error("Couldn't save reward to database: {}".format(e))
